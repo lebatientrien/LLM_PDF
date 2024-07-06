42614abb6c2dd07d5012e397172b10b676b84bc3
Le Ba Tien Trien <lbtientrien@gmail.com>
2024-07-06 17:45:57 +0700

    Initial libraries setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import torch
+
+from transformers import BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface.llms import HuggingFacePipeline
+
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain.chains import ConversationalRetrievalChain
+
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain import hub
